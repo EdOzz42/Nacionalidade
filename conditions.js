2f53b06e059bd32308c4e60d11db4d424fe92af4
Eduardo de Oliveira <eduardoolive133@gmail.com>
2022-11-05 22:15:19 -0300

    O básico
    Site que tem a finalidade boba de vereficar se você é brasileiro ou não.

--- conditions.js
+++ conditions.js
@@ -5,9 +5,9 @@ function vereficar() {
     let input0 = document.querySelector('input#txt0')
     let res = document.querySelector('div#Resultado')
     let nação = String(input0.value) 
-        if (nação == 'Brasil') {
-            res.innerHTML = 'Sim'
+        if (nação == 'Brasil' || nação == 'Brazil' || nação == 'brasil' || nação == 'brazil') {
+            res.innerHTML = 'Você é <strong>brasileiro</strong>' 
         } else {
-            res.innerHTML = 'Nope'
+            res.innerHTML = `Você é <strong>gringo</strong>, pelo fato de você ter nascido no país: <strong>${nação}</strong>`
         }
-} 
+    } 
