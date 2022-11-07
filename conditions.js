8a6099276ebdd2217243061e179ddf3859263631
Eduardo de Oliveira <eduardoolive133@gmail.com>
2022-11-07 03:05:27 -0300

    Atualização
    Aprimoramento no estilo e interatividade.

--- conditions.js
+++ conditions.js
@@ -1,5 +1,7 @@
 var pres = window.document.querySelector('input#botao')
 pres.addEventListener('click', vereficar)
+pres.addEventListener('mouseenter', entrar)
+pres.addEventListener('mouseout', saiu)
 
 function vereficar() {
     let input0 = document.querySelector('input#txt0')
@@ -11,3 +13,14 @@ function vereficar() {
             res.innerHTML = `Você é <strong>gringo</strong>, pelo fato de você ter nascido no país: <strong>${nação}</strong>`
         }
     } 
+
+function entrar() {
+    pres.style.height = "36px"
+    pres.style.width = "255px"
+
+}
+
+function saiu() {
+    pres.style.height = "33px"
+    pres.style.width = "232px"
+}
