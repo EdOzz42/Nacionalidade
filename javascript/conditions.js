29d9684ea365a2c2fa487bac87f2717f67a4eb21
Eduardo de Oliveira <eduardoolive133@gmail.com>
2022-11-23 04:30:31 -0300

    Mudanças no estilo

--- javascript/conditions.js
+++ javascript/conditions.js
@@ -5,13 +5,27 @@ pres.addEventListener('mouseout', saiu)
 
 function vereficar() {
     let input0 = document.querySelector('input#txt0')
-    let res = document.querySelector('section#Resultado')
+    let res = document.querySelector('p#Resultado')
     let nação = String(input0.value) 
-        if (nação == 'Brasil' || nação == 'Brazil' || nação == 'brasil' || nação == 'brazil') {
-            res.innerHTML = 'Você é <strong>brasileiro</strong>' 
-        } else {
-            res.innerHTML = `Você é <strong>gringo</strong>, pelo fato de você ter nascido no país: <strong>${nação}</strong>`
-        }
+    let image = document.querySelector('#image')
+
+    if (nação.length == 0) {
+        window.alert('[ERRO!] Digite um país válido!')
+    }
+    else if (nação == 'Brasil' || nação == 'Brazil' || nação == 'brasil' || nação == 'brazil' || nação == 'Federative Republic of Brazil' || nação ==  'República Federativa do Brasil') {
+        res.innerHTML = 'Você é <strong>brasileiro</strong>'
+        image.setAttribute('src', 'images/bra.png')  
+        image.style.marginTop = '10px' 
+        image.style.width = '158'
+        image.style.height = '158'
+    } 
+    else {
+        res.innerHTML = `Você é <strong>gringo</strong>, pelo fato de você ter nascido no país: <strong>${nação}</strong>`
+        image.setAttribute('src', 'images/world.png')
+        image.style.marginTop = '10px' 
+        image.style.width = '145'
+        image.style.height = '145'
+    }
     } 
 
 function entrar() {
